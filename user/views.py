aa0aae98959575aa161420187f049a74547171c3
mehdizhd11 <98504734+mehdizhd11@users.noreply.github.com>
2025-11-15 22:47:02 +0330

    user : ( test user api done ) change docker files

--- user/views.py
+++ user/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from .serializers import CustomTokenObtainPairSerializer, UserRegistrationSerializer, UserSerializer
 
 
