10a38353ab456615458743b4e28c6947e3fbc37c
mehdizhd11 <98504734+mehdizhd11@users.noreply.github.com>
2025-11-30 15:53:58 +0330

    settings: (change settings)

--- software_suite/settings.py
+++ software_suite/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user'
+    'user',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
